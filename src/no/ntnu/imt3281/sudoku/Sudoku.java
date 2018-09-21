778459fa89146801814a268afb5c905ef2e1b0a9
BredeFK <bredefk@stud.ntnu.no>
2018-09-21 13:24:10 +0200

    Checks rows and columns

--- src/no/ntnu/imt3281/sudoku/Sudoku.java
+++ src/no/ntnu/imt3281/sudoku/Sudoku.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Sudoku extends Application  {
+	private static final int NUMB_ROW = 9;
+	private static final int NUMB_COLUMN = 9;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -18,7 +22,7 @@ public class Sudoku extends Application  {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("SudukoView.fxml"));
 		Scene scene = new Scene(root);
-		primaryStage.setTitle("Suduko");
+		primaryStage.setTitle("Velkommen til Suduko");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
--- src/no/ntnu/imt3281/sudoku/SudukoViewController.java
+++ src/no/ntnu/imt3281/sudoku/SudukoViewController.java
@@ -1,11 +1,31 @@
 package no.ntnu.imt3281.sudoku;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+
+import javax.swing.text.ChangedCharSetException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 
 public class SudukoViewController {
+	
+	private static final int NUMB_ROW = 9;
+	private static final int NUMB_COLUMN = 9;
+	private TextField[][] textFields;
+	 
+	public SudukoViewController() {
+		 textFields = new TextField[NUMB_ROW][NUMB_COLUMN];
+	}
+	
+	 @FXML
+	 private BorderPane borderPane;
 
     @FXML
     private Button btn_Easy;
@@ -15,15 +35,91 @@ public class SudukoViewController {
 
     @FXML
     private GridPane gridID;
+    
+    @FXML
+    private ToolBar toolBar;
 
     @FXML
     void onEasyClick(ActionEvent event) {
-    	System.out.println("REEEEEEEEEEEEEEEEEEE - Easy");
+    	System.out.println("Easy button");
+    	generate();
     }
 
     @FXML
     void onMediumClick(ActionEvent event) {
-    	System.out.println("REEEEEEEEEEEEEEEEEEE - Medium");
+    	System.out.println("Medium button");
+    }
+    
+    private void generate() {
+    	GridPane grid = new GridPane();
+		for(int row = 0; row < NUMB_ROW; row++) {
+			for(int col = 0; col < NUMB_COLUMN; col++) {
+				
+				TextField field = new TextField();
+				field.setMinHeight((borderPane.getHeight()-toolBar.getHeight())/NUMB_COLUMN);
+				field.setMaxWidth(borderPane.getWidth()/NUMB_ROW);
+				textFields[row][col] = field;
+				
+				grid.add(field, col, row);
+				
+				final int selectedRow = row;
+				final int selectedCol = col;
+				
+				textFields[row][col].textProperty().addListener(new ChangeListener<String>() {
+					@Override
+					public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+						
+						// Make sure input isn't empty and is an number
+						if(!newValue.isEmpty() && newValue.matches("\\d+")) {
+							
+								// Parse to int
+								int number = Integer.parseInt(newValue);
+								
+								// Make sure its the right input
+								if(number >= 1 && number <= 9) {
+									
+									// Check rows, columns and boxes
+									check(selectedRow, selectedCol, newValue);
+									
+								} else {
+									textFields[selectedRow][selectedCol].setText("");
+								}
+							} else {
+								textFields[selectedRow][selectedCol].setText("");
+							}
+					}
+				});
+				
+			}
+			
+		}
+		
+		borderPane.setCenter(grid);
+    }
+    
+    private void check(int row, int col, String value) {
+    	// Check row
+    	for(int i = 0; i < NUMB_COLUMN; i++) {
+    		if(col != i) {
+	    		if(textFields[row][i].getText().equals(value)){
+	    			textFields[row][col].setText("");
+	    			System.out.println("Row: nei");
+	    		} 
+    		}
+    	}
+    	
+    	// Check column
+    	for(int j = 0; j < NUMB_ROW; j++) {
+    		if(row != j) {
+    			if(textFields[j][col].getText().equals(value)) {
+    				textFields[row][col].setText("");
+    				System.out.println("Col: nei");
+    			}
+    		}
+    	}
+    	
+    	// Check box
+    	
     }
 
 }
